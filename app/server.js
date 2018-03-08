de15bf42dc0443ea01d715a1fdb39ad97d560896
MC <chuyik@gmail.com>
2018-03-08 18:56:41 +0800

    updated implementation of MyCryptoRouter

--- app/server.js
+++ app/server.js
@@ -1,10 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 class MyCryptoServer {
-	constructor() {
-		
+	constructor(router) {
+		this.router = router;
+		this.app = express();
 	}
 
-	start() {
+	start(callback) {
+		this.app.use(bodyParser.json());
+		this.app.route('/:target/:base').get(this.router.getCryptoPair);
+		this.app.use('/', this.router.send404);
+		this.app.listen(8888, () => {
+			console.log('Server started, listening on port 8888');
+			callback();
+		});
 	}
+
+	get server() { return this.app; }
 }
 
 module.exports = MyCryptoServer;
