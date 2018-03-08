8248dccd003b52083dfbcc0366440bda0dd290ee
MC <chuyik@gmail.com>
2018-03-08 18:12:40 +0800

    added test stubs that describe the desired server side logic

--- test/server_test.js
+++ test/server_test.js
@@ -8,7 +8,49 @@ describe('Crypto-monitor server tests', () => {
 		done();
 	});
 
-	it('should run this test correctly', (done) => {
-		done();
+	describe.skip('/GET /:target/:base', () => {
+
+		describe('when queried crypto pair in DB is up to date', () => {
+			it('should return the correct crypto pair from DB', (done) => {
+				done();
+			});
+
+			it('should be case-insensitive', (done) => {
+				done();
+			});
+		});
+
+		describe('when queried crypto pair in DB is not found / has expired', () => {
+			it('should send http get request to third party api', (done) => {
+				done();
+			});
+
+			it('should update the cryto pair in DB when received response data', (done) => {
+				done();
+			});
+
+			it('should return the updated cryto pair info when received response data', (done) => {
+				done();
+			});
+		});
+
+		describe('when queried crypto pair in DB has expired but failed getting api response', () => {
+			it('should return the existing cryto pair from DB', (done) => {
+				done();
+			});
+		});
+
+		describe('when queried crypto pair in DB is not found but failed getting api response', () => {
+			it('should return error', (done) => {
+				done();
+			});
+		});
+
+	});
+
+	describe('Invalid requests', () => {
+		it.skip('should return 404 error', (done) => {
+			done();
+		});
 	});
 });
