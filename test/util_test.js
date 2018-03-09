567678f662729b2cfaab58c520fae69aad17b5c4
MC <chuyik@gmail.com>
2018-03-09 18:18:18 +0800

    updated util test - instead of using a mock object that expires in 2999 now fakes time for the test

--- test/util_test.js
+++ test/util_test.js
@@ -1,12 +1,22 @@
+const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
+const config = require('config');
+const interval = config.get('expireInSec');
 const util = require('../app/util/util');
 
 const CryptoPair = require('../app/model/crypto-pair');
 const CryptoMock = require('./mock/crypto-mock');
 
+var clock;
 describe('Custom util tests', () => {
 	describe('#shouldUpdateFromApi', () => {
+		afterEach('restore time if faked', () => {
+			if (clock) {
+				clock.restore();
+			}
+		});
+
 		it('should return true if doc is null', () => {
 			const doc = null;
 			expect(util.shouldUpdateFromApi(doc)).to.be.true;
@@ -14,11 +24,13 @@ describe('Custom util tests', () => {
 
 		it('should return true if doc has expired', () => {
 			const doc = CryptoPair(CryptoMock.btc_usd);
+			clock = sinon.useFakeTimers({ now: (doc.lastUpdated + interval + 1) * 1000 });
 			expect(util.shouldUpdateFromApi(doc)).to.be.true;
 		});
 
 		it('should return false if doc is up-to-date', () => {
-			const doc = CryptoPair(CryptoMock.btc_usd_never_expire);
+			const doc = CryptoPair(CryptoMock.btc_usd);
+			clock = sinon.useFakeTimers({ now: (doc.lastUpdated + interval) * 1000 });
 			expect(util.shouldUpdateFromApi(doc)).to.be.false;
 		});
 	});
