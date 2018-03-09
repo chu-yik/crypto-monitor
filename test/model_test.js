0d44dd5da147f4a9c0d3ef13f28d8179fbb617c2
MC <chuyik@gmail.com>
2018-03-09 10:24:37 +0800

    created stubs for unit tests of db model

---
+++ test/model_test.js
@@ -0,0 +1,27 @@
+describe('Crypto pair model tests', () => {
+	describe('model validation', () => {
+		it('should be invalid if base is empty', (done) => {
+			done();
+		});
+
+		it('should be invalid if target is empty', (done) => {
+			done();
+		});
+	});
+
+	describe('model initialization', () => {
+		it('should force lowercase on base', (done) => {
+			done();
+		});
+
+		it('should force lowercase on target', (done) => {
+			done();
+		});
+	});
+
+	describe('model to json', () => {
+		it('should return expected info', (done) => {
+			done();
+		});
+	});
+});
