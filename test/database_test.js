0c807dc921f2c4f65f395a6cc78ab087d1044f2b
MC <chuyik@gmail.com>
2018-03-09 11:22:47 +0800

    added stubs for integration tests with mongodb

---
+++ test/database_test.js
@@ -0,0 +1,31 @@
+describe.skip('Database integration tests', () => {
+	describe('Query from mongoose model', () => {
+		it('should be case-insensitive', (done) => {
+			done();
+		});
+
+		it('should return the record if found', (done) => {
+			done();
+		});
+
+		it('should return null if not found', (done) => {
+			done();
+		});
+	});
+
+	describe('Save from mongoose model', () => {
+		it('should throw error for duplicated index', (done) => {
+			done();
+		});
+	});
+
+	describe('Upsert from mongoose model', () => {
+		it('should update the record if existing', (done) => {
+			done();
+		});
+
+		it('should insert the record if not existing', (done) => {
+			done();
+		});
+	});
+});
